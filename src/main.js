8abb297b69a5ae90d34821fca06cf1052b696fe5
ak4code <haritonov.aka@gmail.com>
2020-01-13 18:19:18 +0300

    start project

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import UIkit from 'uikit'
+import '@/assets/styles/styles.scss'
+import Icons from 'uikit/dist/js/uikit-icons'
+
+Vue.config.productionTip = false
+
+const app = new Vue({
+  el: '#app'
+})
