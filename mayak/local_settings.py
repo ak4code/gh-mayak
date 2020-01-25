24760f639d3e4b276bae33f5bde35e89c883e6e6
akweb <haritonov.aka@gmail.com>
2020-01-25 12:19:09 +0300

    settings

--- mayak/local_settings.py
+++ mayak/local_settings.py
@@ -8,3 +8,10 @@ WEBPACK_LOADER = {
         'IGNORE': [r'.+\.hot-update.js', r'.+\.map', r'.+\node_modules']
     }
 }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
