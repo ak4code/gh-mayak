5b6955f7620b93e9333cd8ab0d30a3cbb67c75b6
akweb <haritonov.aka@gmail.com>
2020-01-25 16:31:05 +0300

    api

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -12,6 +12,8 @@ let config = {
   // baseURL: process.env.baseURL || process.env.apiUrl || ""
   // timeout: 60 * 1000, // Timeout
   // withCredentials: true, // Check cross-site Access-Control
+  xsrfCookieName: 'csrftoken',
+  xsrfHeaderName: 'X-CSRFTOKEN',
 }
 
 const _axios = axios.create(config)
